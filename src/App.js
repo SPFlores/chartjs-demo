d5ad20b850f7132f28c32b69e3556d74e86d8024
SPFlores <sflores92886@gmail.com>
2021-04-20 16:18:42 -0700

    adding in links for documentation

--- src/App.js
+++ src/App.js
@@ -16,6 +16,15 @@ const ChartJS = () => {
     <div className='App' style={{ textAlign: 'center', padding: '0 100px' }}>
       <h1>Let's build some charts!</h1>
       <h4>For the video accompanying this code, go <a href='https://drive.google.com/file/d/1nyNRE7D7rphQefSrhPI9wjfyQrD1NoxO/view?usp=sharing' target='_blank' rel='noreferrer'>here</a> to access the Google Drive video</h4>
+      <p>
+        React-Chartjs-2: <a href='https://www.npmjs.com/package/react-chartjs-2' target='_blank' rel='noreferrer'>here</a>
+        <br />
+        Chartjs: <a href='https://www.chartjs.org/docs/latest/' target='_blank' rel='noreferrer'>here</a>
+        <br />
+        Patternomaly: <a href='https://github.com/ashiguruma/patternomaly' target='_blank' rel='noreferrer'>here</a>
+        <br />
+        Chartjs Datalabels Plugin: <a href='https://chartjs-plugin-datalabels.netlify.app/guide/' target='_blank' rel='noreferrer'>here</a>
+      </p>
       <br />
       <button onClick={togglePatterns}>Toggle patterns</button>
       <br />
